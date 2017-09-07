3a93c2c4625b96023e598bca0039add400b91163
Jade Bautista <edaj.bautista@gmail.com>
2017-09-07 13:08:49 +0800

    User list

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import UserList from './user_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Test template</div>
+      <div>
+        <UserList />
+      </div>
     );
   }
 }
